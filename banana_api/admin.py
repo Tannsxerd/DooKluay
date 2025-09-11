f12266c8070f154def75dc12a03874074b4f7659
SNeXT <snext2004@gmail.com>
2025-09-12 00:45:29 +0700

    add models' CRUD to admin.py

--- banana_api/admin.py
+++ banana_api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Disease, ImageUpload, ModelFile, Prediction, Treatment
 
-# Register your models here.
+admin.site.register(Disease)
+admin.site.register(ImageUpload)
+admin.site.register(ModelFile)
+admin.site.register(Prediction)
+admin.site.register(Treatment)
