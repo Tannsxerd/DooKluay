035649eadbcfa0356bc8e61b213e0d72f3c6dd9e
lemuruz <139195200+lemuruz@users.noreply.github.com>
2025-09-12 17:04:48 +0700

    Revert "write feature to get all/single image(from image id)"

--- banana_api/urls.py
+++ banana_api/urls.py
@@ -4,9 +4,7 @@ from .views.treatment import Treatmentlist, TreatmentDetails
 
 urlpatterns = [
     path('disease/', DiseaseList.as_view(), name='disease_List'),
-    path('disease/<int:pk>/', DiseaseDetail.as_view(), name='disease-detail'),# GET /api/diseases/5/ (ตัวอย่างคือดึง disease ที่มี id=5)
     path('treatment/',Treatmentlist.as_view(), name='treatment_list'),
     path('treatment/<int:pk>/', TreatmentDetails.as_view(), name='treatment-detail'),
-    path('image/<int:image_id>/', ImageDetailView.as_view(), name='image_detail'),
-    path('images/',ImageViewALL.as_view(), name='all_image')
+    path('disease/<int:pk>/', DiseaseDetail.as_view(), name='disease-detail'),# GET /api/diseases/5/ (ตัวอย่างคือดึง disease ที่มี id=5)
 ]
