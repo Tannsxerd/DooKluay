762edf6d2bfe490a6f78fd4644a9f8635ad300e9
lemuruz <nakabodeepatiparn@gmail.com>
2025-09-12 17:07:42 +0700

    add import ImageDetailView,ImageViewALL it banana_api/urls

--- banana_api/urls.py
+++ banana_api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views.disease import DiseaseList, DiseaseDetail
 from .views.treatment import Treatmentlist, TreatmentDetails
+from .views.ImageUpload import ImageDetailView,ImageViewALL
 
 urlpatterns = [
     path('disease/', DiseaseList.as_view(), name='disease_List'),
