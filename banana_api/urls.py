6f23a7384cda6159667197669feae95393a9691b
lemuruz <nakabodeepatiparn@gmail.com>
2025-09-12 16:48:33 +0700

    write feature to get all/single image(from image id)

--- banana_api/urls.py
+++ banana_api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views.disease import DiseaseList
+from .views.ImageUpload import ImageDetailView,ImageViewALL
 
 urlpatterns = [
     path('disease/', DiseaseList.as_view(), name='disease_List'),
+    path('image/<int:image_id>/', ImageDetailView.as_view(), name='image_detail'),
+    path('images/',ImageViewALL.as_view(), name='all_image')
 ]
